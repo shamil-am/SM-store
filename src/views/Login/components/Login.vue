f3b684152b43be4fbaaeaf67a6ee889bd78cf361
shamil-am <shamil.mammadov1@gmail.com>
2022-05-13 12:57:40 +0400

    login made by vee validate

--- src/views/Login/components/Login.vue
+++ src/views/Login/components/Login.vue
@@ -3,59 +3,69 @@
     <h2 class="text-center">Daxil ol</h2>
     <SocialButtons />
     <p class="or">və ya</p>
-    <v-form v-model="valid">
-      <v-container>
-        <v-row>
-          <v-col cols="12 pa-0">
-            <span for="">Email</span>
-            <v-text-field
-              v-model="email"
-              :rules="emailRules"
-              required
-              outlined
-              placeholder="Emailinizi daxil edin"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12 pa-0">
-            <span for="">Şifrə</span>
-            <v-text-field
-              v-model="password"
-              :rules="passwordRules"
-              required
-              outlined
-              placeholder="Şifrə daxil edin"
-              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-              :type="show ? 'text' : 'password'"
-              class="input-group--focused"
-              @click:append="show = !show"
-            ></v-text-field>
-          </v-col>
-          <div class="forgot-password">
-            <a href="http://" target="_blank">Şifrəmi unutdum</a>
-          </div>
-          <v-btn block color="black" dark> Daxil ol </v-btn>
-        </v-row>
-      </v-container>
-    </v-form>
+    <ValidationObserver v-slot="{ handleSubmit }">
+      <v-form v-model="valid">
+        <v-container>
+          <v-row>
+            <v-col cols="12 pa-0">
+              <ValidationProvider name="email" rules="required|email" v-slot="{ errors }">
+                <v-text-field v-model="email" :error-messages="errors" outlined placeholder="Emailinizi daxil edin"></v-text-field>
+              </ValidationProvider>
+            </v-col>
+            <v-col cols="12 pa-0">
+              <ValidationProvider name="password" rules="required" v-slot="{ errors }">
+                <v-text-field
+                  v-model="password"
+                  :error-messages="errors"
+                  outlined
+                  placeholder="Şifrə daxil edin"
+                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="show ? 'text' : 'password'"
+                  class="input-group--focused"
+                  @click:append="show = !show"
+                ></v-text-field>
+              </ValidationProvider>
+            </v-col>
+            <div class="forgot-password">
+              <a href="http://" target="_blank">Şifrəmi unutdum</a>
+            </div>
+            <v-btn block color="black" dark @click="handleSubmit(submit)"> Daxil ol </v-btn>
+          </v-row>
+        </v-container>
+      </v-form>
+    </ValidationObserver>
   </v-col>
 </template>
 
 <script>
 import SocialButtons from "./SocialButtons.vue";
+import { ValidationObserver, ValidationProvider, extend } from "vee-validate";
+import { email, required } from "vee-validate/dist/rules";
+
+extend("email", {
+  ...email,
+  message: "Yalnis email formati",
+});
+extend("required", {
+  ...required,
+  message: "Zehmet olmasa qeyd edin",
+});
 export default {
   components: {
     SocialButtons,
+    ValidationObserver,
+    ValidationProvider,
   },
   data: () => ({
     valid: false,
     email: "",
-    emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+/.test(v) || "E-mail must be valid",
-    ],
     show: false,
     password: "",
-    passwordRules: [(v) => !!v || "Şifrə daxil edin"],
   }),
+  methods: {
+    async submit() {
+      alert("submitting!");
+    },
+  },
 };
 </script>
